bc881a5337c5cf42c2e2eb589f834f65bc69e1ab
mleitner8 <molly.leitner@downstate.edu>
2025-07-01 14:34:46 -0400

    added cfg for variants

--- src/cfg.py
+++ src/cfg.py
@@ -8,7 +8,6 @@ Contributors: salvadordura@gmail.com
 
 from netpyne import specs
 import pickle
-import pandas as pd
 
 cfg = specs.SimConfig()
 
@@ -22,14 +21,13 @@ cfg = specs.SimConfig()
 # ------------------------------------------------------------------------------
 # Run parameters
 # ------------------------------------------------------------------------------
-cfg.duration = 2000
-cfg.transient = 1000 # what is this for?
+cfg.duration = 1500
 cfg.dt = 0.025
 cfg.seeds = {'conn': 4321, 'stim': 1234, 'loc': 4321}
 cfg.hParams = {'celsius': 34, 'v_init': -80}
-cfg.verbose = 0
-cfg.createNEURONObj = 1
-cfg.createPyStruct = 1
+cfg.verbose = False
+cfg.createNEURONObj = True
+cfg.createPyStruct = True
 cfg.connRandomSecFromList = False  # set to false for reproducibility
 cfg.cvode_active = False
 cfg.cvode_atol = 1e-6
@@ -38,15 +36,19 @@ cfg.printRunTime = 0.1
 cfg.printSynsAfterRule = False
 cfg.pt3dRelativeToCellLocation = True
 cfg.oneSynPerNetcon = True  # only affects conns not in subconnParams; produces identical results
+cfg.progressBar = 0
 
-cfg.includeParamsLabel = False  # True # needed for modify synMech False
-cfg.printPopAvgRates = [cfg.transient, cfg.duration]
+cfg.includeParamsLabel = False
+cfg.printPopAvgRates = [0, cfg.duration]
 
 cfg.checkErrors = False
 cfg.checkErrorsVerbose = False
+cfg.validateNetParams = True
 
-cfg.saveInterval = 100  # define how often the data is saved, this can be used with interval run if you want to update the weights more often than you save
-cfg.intervalFolder = 'interval_saving'
+# cfg.rand123GlobalIndex = None
+# cfg.coreneuron = True
+# cfg.random123 = True
+# cfg.gpu = True
 
 # ------------------------------------------------------------------------------
 # Recording
@@ -56,9 +58,9 @@ allpops = ['NGF1', 'IT2', 'PV2', 'SOM2', 'VIP2', 'NGF2',
            'IT5A', 'PV5A', 'SOM5A', 'VIP5A', 'NGF5A',
            'IT5B', 'PT5B', 'PV5B', 'SOM5B', 'VIP5B', 'NGF5B',
            'IT6', 'CT6', 'PV6', 'SOM6', 'VIP6', 'NGF6']
-cfg.cellsrec = 1
+cfg.cellsrec = 0
 if cfg.cellsrec == 0:
-    cfg.recordCells = ['all']  # record all cells
+    cfg.recordCells = ['PT5B']  # all record all cells
 elif cfg.cellsrec == 1:
     cfg.recordCells = [(pop, 0) for pop in allpops]  # record one cell of each pop
 elif cfg.cellsrec == 2:
@@ -72,7 +74,8 @@ elif cfg.cellsrec == 4:
                       + [('PT5B', x) for x in [393, 447, 579, 19, 104, 214, 1138, 979, 799]]  # record selected cells
 
 cfg.recordTraces = {'V_soma': {'sec': 'soma', 'loc': 0.5, 'var': 'v'},
-                    'V_axon_0': {'sec': 'axon_0', 'loc': 0.5, 'var': 'v', 'conds': {'pop': 'PT5B'}}}
+                    'V_axon0': {'sec': 'axon_0', 'loc': 0.5, 'var': 'v'}}  # ,
+# 'V_apic_23': {'sec':'apic_23', 'loc':0.5, 'var':'v', 'conds':{'pop': 'PT5B'}},
 # 'V_apic_26': {'sec':'apic_26', 'loc':0.5, 'var':'v', 'conds':{'pop': 'PT5B'}},
 # 'V_dend_5': {'sec':'dend_5', 'loc':0.5, 'var':'v', 'conds':{'pop': 'PT5B'}}}
 # 'I_AMPA_Adend2': {'sec':'Adend2', 'loc':0.5, 'synMech': 'AMPA', 'var': 'i'}}
@@ -81,16 +84,49 @@ cfg.recordTraces = {'V_soma': {'sec': 'soma', 'loc': 0.5, 'var': 'v'},
 
 cfg.recordStim = False
 cfg.recordTime = False
-cfg.recordStep = 0.1
+cfg.recordStep = cfg.dt  # To avoid some numerical errors sometimes
+
+# ------------------------------------------------------------------------------
+# Drug Effects
+# ------------------------------------------------------------------------------
+cfg.treatment = False
+cfg.sodiumMechs = ['na12', 'na12mut', 'Nafx', 'nax', 'na16mut', 'Nafcr', 'ch_Navngf', 'na16', 'na16mut',
+                   'nap']  # Look at the suffix in the modfiles
+cfg.LVACaMechs = ['Ca_LVAst', 'cat', 'catt', 'catcb']
+cfg.variables = ['gbar', 'gnafbar', 'gmax']  # Name of the variable/s to modify
+cfg.drugEffect = 0.5  # Multiplicative factor
+
+# IT2_reduced dict_keys(['cadad', 'cal', 'can', 'cat', 'ih', 'kBK', 'kap', 'kdr', 'nax', 'pas'])
+# IT4_reduced dict_keys(['cadad', 'cal', 'can', 'cat', 'ih', 'kBK', 'kap', 'kdr', 'nax', 'pas'])
+# IT5A_reduced dict_keys(['cadad', 'cal', 'can', 'cat', 'ih', 'kBK', 'kap', 'kdr', 'nax', 'pas'])
+# IT5B_reduced dict_keys(['cadad', 'cal', 'can', 'cat', 'ih', 'kBK', 'kap', 'kdr', 'nax', 'pas'])
+# PT5B_reduced dict_keys(['cadad', 'cal', 'can', 'ih', 'kBK', 'kap', 'kdmc', 'kdr', 'nax', 'pas'])
+# IT6_reduced dict_keys(['cadad', 'cal', 'can', 'cat', 'ih', 'kBK', 'kap', 'kdr', 'nax', 'pas'])
+# CT6_reduced dict_keys(['cadad', 'cal', 'can', 'cat', 'ih', 'kBK', 'kap', 'kdr', 'nax', 'pas'])
+# SOM_reduced dict_keys(['Nafx', 'cadyn', 'catcb', 'hin', 'kapcb', 'kdrin', 'pas'])
+# IT5A_full dict_keys(['cadad', 'cal', 'can', 'cat', 'ican', 'ih', 'kBK', 'kap', 'kdr', 'nap', 'nax', 'pas'])
+# PT5B_full dict_keys(['CaDynamics_E2', 'Ca_HVA', 'Ca_LVAst', 'Ih', 'SK_E2', 'SKv3_1', 'na12', 'na12mut', 'na16', 'na16mut', 'pas'])
+# PV_reduced dict_keys(['IKsin', 'Nafx', 'cadyn', 'canin', 'hin', 'kapin', 'kctin', 'kdrin', 'pas'])
+# VIP_reduced dict_keys(['IKscr', 'Nafcr', 'cadyn', 'cancr', 'iCcr', 'kdrcr', 'pas'])
+# NGF_reduced dict_keys(['ch_CavL', 'ch_CavN', 'ch_KCaS', 'ch_Kdrfastngf', 'ch_KvAngf', 'ch_KvCaB', 'ch_Navngf', 'hd', 'iconc_Ca', 'pas'])
+
+# ------------------------------------------------------------------------------
+# Variants and specifics of Tim's PT5B model
+# ------------------------------------------------------------------------------
+
+cfg.dendNa = 0.1
+cfg.loadmutantParams = False
+cfg.variant = 'WT'  # L1666F, E1211K, D195G, R853Q, K1422E, M1879T, WT
 
 # ------------------------------------------------------------------------------
 # Saving
 # ------------------------------------------------------------------------------
-cfg.simLabel = 'v103_tune1'
-cfg.saveFolder = '../batchData/v103_manualTune'
-cfg.savePickle = True
+cfg.simLabel = 'v103_tune_%s_treatment_%s' % (
+cfg.variant, str(cfg.treatment)) if cfg.loadmutantParams else 'v103_tune_WT_treatment%s' % str(cfg.treatment)
+cfg.saveFolder = '../data/v103_manualTune'
+cfg.savePickle = False
 cfg.saveJson = True
-cfg.saveDataInclude = ['simData', 'simConfig', 'netParams']  # , 'net']
+cfg.saveDataInclude = ['simData', 'simConfig']  # , 'netParams'] #, 'net']
 cfg.backupCfgFile = None  # ['cfg.py', 'backupcfg/']
 cfg.gatherOnlySimData = False
 cfg.saveCellSecs = False
@@ -101,14 +137,26 @@ cfg.compactConnFormat = 0
 # Analysis and plotting
 # ------------------------------------------------------------------------------
 with open('../cells/popColors.pkl', 'rb') as fileObj: popColors = pickle.load(fileObj)['popColors']
-
 cfg.analysis['plotRaster'] = {'include': allpops, 'orderBy': ['pop', 'y'], 'timeRange': [0, cfg.duration],
                               'saveFig': True, 'showFig': False, 'popRates': True, 'orderInverse': True,
-                              'popColors': popColors, 'figSize': (12, 10), 'lw': 0.3, 'markerSize': 3, 'marker': '.',
-                              'dpi': 300}
+                              'popColors': popColors, 'figSize': (18, 18), 'lw': 0.3, 'markerSize': 3, 'marker': '.',
+                              'dpi': 100}
+
+# cfg.analysis['plotSpikeHist'] = {'include': ['IT2','IT4','IT5A','IT5B','PT5B','IT6','CT6'], 'timeRange': [1000,6000], 'yaxis':'rate', 'binSize':5, 'graphType':'bar',
+#  								'saveFig': True, 'showFig': False, 'popColors': popColors, 'figSize': (10,4), 'dpi': 300}
+
+# cfg.analysis['plotLFP'] = {'plots': ['spectrogram'], 'figSize': (6,10), 'timeRange': [1000,6000], 'NFFT': 256*20, 'noverlap': 128*20, 'nperseg': 132*20,
+# 							'saveFig': True, 'showFig':False}
 
 cfg.analysis['plotTraces'] = {'include': cfg.recordCells, 'timeRange': [0, cfg.duration], 'overlay': True,
                               'oneFigPer': 'trace', 'figSize': (10, 4), 'saveFig': True, 'showFig': False}
+
+# cfg.analysis['plotShape'] = {'includePre': ['all'], 'includePost': [('PT5B',100)], 'cvar':'numSyns','saveFig': True, 'showFig': False, 'includeAxon': False}
+# cfg.analysis['plotConn'] = {'include': ['allCells']}
+# cfg.analysis['calculateDisynaptic'] = True
+
+# cfg.analysis['plotConn'] = {'includePre': allpops, 'includePost': allpops, 'feature': 'strength', 'figSize': (10,10), 'groupBy': 'pop', \
+#  						'graphType': 'bar', 'synOrConn': 'conn', 'synMech': None, 'saveData': None, 'saveFig': 1, 'showFig': 0}
 
 # ------------------------------------------------------------------------------
 # Cells
@@ -133,19 +181,11 @@ cfg.ihSlope = 14 * 2
 
 cfg.removeNa = False  # simulate TTX; set gnabar=0s
 cfg.somaNa = 5
-cfg.dendNa = 0.3
 cfg.axonNa = 7
 cfg.axonRa = 0.005
 
 cfg.gpas = 0.5  # multiplicative factor for pas g in PT cells
 cfg.epas = 0.9  # multiplicative factor for pas e in PT cells
-
-cfg.KgbarFactor = 1.0  # multiplicative factor for K channels gbar in all E cells
-cfg.makeKgbarFactorEqualToNewFactor = False
-
-cfg.modifyMechs = {'startTime': 500, 'endTime': 1000, 'cellType':'PT', 'mech': 'hd', 'property': 'gbar', 'newFactor': 1.00, 'origFactor': 0.75}
-
-cfg.PTNaFactor = 1
 
 # ------------------------------------------------------------------------------
 # Synapses
@@ -153,7 +193,7 @@ cfg.PTNaFactor = 1
 cfg.synWeightFractionEE = [0.5, 0.5]  # E->E AMPA to NMDA ratio
 cfg.synWeightFractionEI = [0.5, 0.5]  # E->I AMPA to NMDA ratio
 cfg.synWeightFractionSOME = [0.9, 0.1]  # SOM -> E GABAASlow to GABAB ratio
-cfg.synWeightFractionNGF = [0.5, 0.5] # NGF GABAA to GABAB ratio
+cfg.synWeightFractionNGF = [0.5, 0.5]  # NGF GABAA to GABAB ratio
 
 cfg.synsperconn = {'HH_full': 5, 'HH_reduced': 1, 'HH_simple': 1}
 cfg.AMPATau2Factor = 1.0
@@ -171,42 +211,39 @@ cfg.weightNormThreshold = 4.0  # weight normalization factor threshold
 cfg.addConn = 1
 cfg.allowConnsWithWeight0 = True
 cfg.allowSelfConns = False
-cfg.scale = 1
+cfg.scale = 1.0
 cfg.sizeY = 1350.0
 cfg.sizeX = 300.0
 cfg.sizeZ = 300.0
-cfg.scaleDensity = 1
+cfg.scaleDensity = 0.3
 cfg.correctBorderThreshold = 150.0
 
 cfg.L5BrecurrentFactor = 1.0
 cfg.ITinterFactor = 1.0
 cfg.strengthFactor = 1.0
 
-cfg.EEGain = 0.9701458851851824
+cfg.EEGain = 1.0
 cfg.EIGain = 1.0
 cfg.IEGain = 1.0
 cfg.IIGain = 1.0
 
 ## E->I by target cell type
-cfg.EICellTypeGain= {'PV': 2.588295268601415, 'SOM': 0.6568380849927258, 'VIP': 1.4582025338644486, 'NGF': 3.355557614291127}
+cfg.EICellTypeGain = {'PV': 1.0, 'SOM': 1.0, 'VIP': 1.0, 'NGF': 1.0}
 
 cfg.IEdisynapticBias = None  # increase prob of I->Ey conns if Ex->I and Ex->Ey exist
 
-cfg.dendNa = 1.0
-
-
 # ------------------------------------------------------------------------------
-## E->I gains (deprecated)
+## (deprecated) E->I gains
 cfg.EPVGain = 1.0
 cfg.ESOMGain = 1.0
 
 # ------------------------------------------------------------------------------
-## I->E gains (deprecated)
+## (deprecated) I->E gains
 cfg.PVEGain = 1.0
 cfg.SOMEGain = 1.0
 
 # ------------------------------------------------------------------------------
-## I->I gains
+## (deprecated) I->I gains
 cfg.PVSOMGain = None  # 0.25
 cfg.SOMPVGain = None  # 0.25
 cfg.PVPVGain = None  # 0.75
@@ -214,8 +251,8 @@ cfg.SOMSOMGain = None  # 0.75
 
 # ------------------------------------------------------------------------------
 ## I->E/I layer weights (L2/3+4, L5, L6)
-cfg.IEweights = [0.8680123131812839, 1.2407609382707516, 1.4735035804798486]
-cfg.IIweights = [1.2917372737477644, 1.0922011258575448, 0.6388172421357348]
+cfg.IEweights = [1.0, 1.0, 1.0]
+cfg.IIweights = [1.0, 1.0, 1.0]
 
 cfg.IPTGain = 1.0
 cfg.IFullGain = 1.0  # deprecated
@@ -229,18 +266,19 @@ cfg.addSubConn = True
 # Long range inputs
 # ------------------------------------------------------------------------------
 cfg.addLongConn = 1
-cfg.numCellsLong = 1000  # num of cells per population
+cfg.numCellsLong = int(1000 * cfg.scaleDensity)  # num of cells per population
 cfg.noiseLong = 1.0  # firing rate random noise
 cfg.delayLong = 5.0  # (ms)
 factor = 1
-cfg.weightLong = {'TPO': 0.45644931218749174, 'TVL': 0.5, 'S1': 0.36622440689049585, 'S2': 0.6784051400778675, 'cM1': 0.5102084809387325, 'M2': 0.2963005602762849, 'OC': 0.5144172551297354}  # corresponds to unitary connection somatic EPSP (mV)
+cfg.weightLong = {'TPO': 0.5 * factor, 'TVL': 0.5 * factor, 'S1': 0.5 * factor, 'S2': 0.5 * factor, 'cM1': 0.5 * factor,
+                  'M2': 0.5 * factor, 'OC': 0.5 * factor}  # corresponds to unitary connection somatic EPSP (mV)
 cfg.startLong = 0  # start at 0 ms
-cfg.ratesLong = {'TPO': [0,5], 'TVL': [0,5], 'S1': [0,5], 'S2': [0,5], 'cM1': [0,5], 'M2': [0,5], 'OC': [0,5]}
+cfg.ratesLong = {'TPO': [0, 5], 'TVL': [0, 2.5], 'S1': [0, 5], 'S2': [0, 5], 'cM1': [0, 5], 'M2': [0, 5], 'OC': [0, 5]}
 
 ## input pulses
-cfg.addPulses = False
-cfg.pulse = {'pop': 'None', 'start': 1000, 'end': 1200, 'rate': [0, 20], 'noise': 0.8}
-cfg.pulse2 = {'pop': 'None', 'start': 1000, 'end': 1200, 'rate': [0, 20], 'noise': 0.5, 'duration': 500}
+cfg.addPulses = 1
+cfg.pulse = {'pop': 'None', 'start': 1000, 'end': 1100, 'rate': 20, 'noise': 0.8}
+cfg.pulse2 = {'pop': 'None', 'start': 1000, 'end': 1200, 'rate': 20, 'noise': 0.5, 'duration': None}
 
 # ------------------------------------------------------------------------------
 # Current inputs
@@ -255,12 +293,8 @@ cfg.IClamp1 = {'pop': 'IT5B', 'sec': 'soma', 'loc': 0.5, 'start': 0, 'dur': 1000
 cfg.addNetStim = 0
 
 ## pop, sec, loc, synMech, start, interval, noise, number, weight, delay
-cfg.NetStim1 = {'pop': 'IT5B', 'sec': 'soma', 'loc': 0.5, 'synMech': ['AMPA', 'NMDA'],
-                'synMechWeightFactor': [0.5, 0.5],
-                'start': 0, 'interval': 1000.0 / 40.0, 'noise': 0.0, 'number': 1000.0, 'weight': 0.5, 'delay': 0}
-
-# ------------------------------------------------------------------------------
-# Load mutant params from csv
-# ------------------------------------------------------------------------------
-cfg.loadmutantParams = True
-cfg.variant = 'R119I'
+# cfg.NetStim1 = {'pop': 'IT2', 'sec': 'soma', 'loc': 0.5, 'synMech': ['AMPA','NMDA'], 'synMechWeightFactor': cfg.synWeightFractionEE,
+# 				'start': 500, 'interval': 50.0, 'noise': 0.2, 'number': 1000.0/50.0, 'weight': 10.0, 'delay': 1}
+cfg.NetStim1 = {'pop': 'IT2', 'ynorm': [0, 1], 'sec': 'soma', 'loc': 0.5, 'synMech': ['AMPA'],
+                'synMechWeightFactor': [1.0],
+                'start': 500, 'interval': 1000.0 / 60.0, 'noise': 0.0, 'number': 60.0, 'weight': 30.0, 'delay': 0}
