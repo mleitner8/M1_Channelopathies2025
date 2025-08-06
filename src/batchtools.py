d4c60bed0778290eaae50bdf2591749b80712f9b
mleitner8 <molly.leitner@downstate.edu>
2025-08-06 14:16:04 -0400

    updated batchtools for expanse

--- src/batchtools.py
+++ src/batchtools.py
@@ -39,13 +39,13 @@ search(job_type = 'sge', # or sh
        max_concurrent = 1)
 """
 
-# EXPANSE CONFIG
+# EXPANSE CONFIG   -- need to change conda environment and add source
 setup = """
 source ~/.bashrc
-source ~/default.sh
-conda activate M1_batchTools
-export LD_LIBRARY_PATH="/home/mollyleitner/miniconda3/envs/NETPYNE/lib/python3.13/site-packages/mpi4py/"
+conda activate py312
+export LD_LIBRARY_PATH="/ddn/mollyleitner/miniconda3/envs/py312/lib/python3.12/site-packages/mpi4py_mpich.libs/"
 """
+
 slurm_config = {
     'allocation': 'TG-MED240058',
     'realtime': '10:30:00',
@@ -55,22 +55,22 @@ slurm_config = {
     'partition': 'compute',
     'email': 'molly.leitner@downstate.edu',
     'custom': setup,
-    'command':'time mpirun -n 96 nrniv -python -mpi init.py'
+    'command': 'time mpirun -n 96 nrniv -python -mpi init.py'
 }
 
-results = search(job_type = 'slurm', # or 'sh'
-       comm_type = 'ssh', # if a metric and mode is specified, some method of communicating with the host needs to be defined
-       label = 'optuna',
+results = search(job_type = 'ssh_slurm', # or 'sh'
+       comm_type = 'sftp', # if a metric and mode is specified, some method of communicating with the host needs to be defined
+       label = 'grid',
        params = params,
-       output_path = '../batchData/optuna_batch',
+       output_path = '../batchData/grid_batch',
        checkpoint_path = '../batchData/ray',
        run_config = slurm_config,
        metric = 'loss', # if a metric and mode is specified, the search will collect metric data and report on the optimal configuration
        mode = 'min',
-       algorithm = "optuna",
+       algorithm = "grid",
        max_concurrent = 1,
        remote_dir='/home/mleitner/M1_Channelopathies2025/src',
        host='expanse0',
-       key='###',
-       num_samples=1,
+       key='',
+       num_samples=2,
        )
