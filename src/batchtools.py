2810adf8d4f952a7dd9fd02bfc93cb5bcb033869
mleitner8 <molly.leitner@downstate.edu>
2026-02-04 14:11:14 -0500

    updated batch tools for algorithms

--- src/batchtools.py
+++ src/batchtools.py
@@ -1,6 +1,11 @@
 from netpyne.batchtools.search import search
 
-params = {
+params = {'weightLong.TPO': [0.25, 0.75],
+          'weightLong.S1': [0.25, 0.75],
+          'weightLong.S2': [0.25, 0.75],
+          'weightLong.cM1': [0.25, 0.75],
+          'weightLong.M2': [0.25, 0.75],
+          'weightLong.OC': [0.25, 0.75],
           'EEGain': [0.5, 1.5],
           'IEweights.0': [0.5, 1.5],
           'IEweights.1': [0.5, 1.5],
@@ -10,76 +15,62 @@ params = {
           'IIweights.2': [0.5, 1.5],
           }
 """
-# use batch_sge_config if running on a
+# SGE GPU CONFIG
 sge_config = {
-    'queue': 'cpu.q',
-    'cores': 64,
-    'vmem': '120G',
+    'queue': 'gpu.q',
+    'cores': 19,
+    'vmem': '90G', #90G
     'realtime': '15:00:00',
-    'command': 'mpiexec -n $NSLOTS -hosts $(hostname) nrniv -python -mpi init_batch.py'}
-
+    'command': 'mpiexec -n $NSLOTS -hosts $(hostname) ./x86_64/special -python -mpi init.py'}
 
 run_config = sge_config
 
-search(job_type = 'sge', # or 'sh'
+search(job_type = 'sge', # or sh
        comm_type = 'socket',
-       label = 'ihGbar',
+       label = 'optuna',
        params = params,
-       output_path = '../grid_batch',
-       checkpoint_path = '../ray',
+       output_path = '../batchData/optuna_batch',
+       checkpoint_path = '../batchData/ray',
        run_config = run_config,
        num_samples = 1,
        metric = 'loss',
        mode = 'min',
-       algorithm = "variant_generator",
-       max_concurrent = 9)
+       algorithm = 'optuna',
+       max_concurrent = 1)
 """
 
-# EXPANSE CONFIG   -- need to change conda environment and add source
-CONFIG_EXPANSE_CPU = """
+# EXPANSE CONFIG
+setup = """
 source ~/.bashrc
-module purge
-module load shared
-module load cpu/0.17.3b
-module load slurm
-module load sdsc
-module load DefaultModules
-module load openmpi/mlnx/gcc/64/4.1.5a1
-conda activate py312
+source ~/default.sh
+conda activate M1_batchTools
+export LD_LIBRARY_PATH="/home/mollyleitner/miniconda3/envs/NETPYNE/lib/python3.13/site-packages/mpi4py/"
 """
+slurm_config = {
+    'allocation': '',
+    'realtime': '10:30:00',
+    'nodes': 1,
+    'coresPerNode': 96,
+    'mem': '128G',
+    'partition': 'compute',
+    'email': 'molly.leitner@downstate.edu',
+    'custom': setup,
+    'command':'time mpirun -n 96 nrniv -python -mpi init.py'
+}
 
-ssh_expanse_cpu = {
-    'job_type': 'ssh_slurm', # or 'sh'
-    'comm_type': 'sftp',
-    'host': 'expanse0',
-    'key':'Z45LIALPT7ZGC4YG7LPJHH7NXQFD4TYF',
-    'remotedir': 'home/mleitner/M1_Channelopathies2025',
-    'output_path': '../batchData/grid_batch',
-    'checkpoint_path': '../batchData/ray',
-    'run_config': {
-        'allocation': 'TG-MED240058',
-        'realtime': '10:30:00',
-        'nodes': 1,
-        'coresPerNode': 96,
-        'mem': '128G',
-        'email': 'molly.leitner@downstate.edu',
-        'command': f"""
-        {CONFIG_EXPANSE_CPU}
-        mpirun -n $SLURM_NTASKS nrniv -python -mpi init.py
-        """
-    }
-    }
-
-
-run_config = ssh_expanse_cpu
-
-search(
-    label='grid_1',
-    params=params,
-    metric = 'loss',
-    mode = 'min',
-    algorithm='grid',
-    max_concurrent = 9,
-    num_samples= 1,
-    **run_config
-)
+results = search(job_type = 'slurm', # or 'sh'
+       comm_type = 'ssh', # if a metric and mode is specified, some method of communicating with the host needs to be defined
+       label = 'optuna',
+       params = params,
+       output_path = '../batchData/optuna_batch',
+       checkpoint_path = '../batchData/ray',
+       run_config = slurm_config,
+       metric = 'loss', # if a metric and mode is specified, the search will collect metric data and report on the optimal configuration
+       mode = 'min',
+       algorithm = "optuna",
+       max_concurrent = 1,
+       remote_dir='/home/mleitner/M1_Channelopathies2025/src',
+       host='expanse0',
+       key='###',
+       num_samples=1,
+       )
